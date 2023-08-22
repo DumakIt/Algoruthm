2c508fe9f28d434505cfc7b47563d5fbd11880db
Odumak <112146844+DumakIt@users.noreply.github.com>
2023-08-22 22:59:43 +0900

    [level 1] Title: 이상한 문자 만들기, Time: 0.20 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12930. 이상한 문자 만들기/이상한 문자 만들기.js
@@ -0,0 +1,15 @@
+function solution(s) {
+  const answer = s
+    .split(" ")
+    .map((word) => {
+      return word
+        .split("")
+        .map((letter, i) => {
+          return i % 2 === 0 ? letter.toUpperCase() : letter.toLowerCase();
+        })
+        .join("");
+    })
+    .join(" ");
+
+  return answer;
+}
